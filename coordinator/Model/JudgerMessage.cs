e9fe5ce59b1004af048abe8d8df0822e991dbef3
Rynco Maekawa <lynzrand@outlook.com>
2020-07-31 19:35:01 +0800

    feat: Add judger message types in coordinator

---
+++ coordinator/Model/JudgerMessage.cs
@@ -0,0 +1,25 @@
+using Dahomey.Json.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Karenia.Rurikawa.Models.Judger
+{
+    public class ServerMsg { }
+
+
+    [JsonDiscriminator("new_job")]
+    public class NewJobServerMsg : ServerMsg
+    {
+        public string Id { get; set; }
+
+        [JsonPropertyName("pkg_uri")]
+        public string PackageUri { get; set; }
+    }
+
+    public class ClientMsg { }
+
+    [JsonDiscriminator("job_result")]
+    public class JobResultMsg : ClientMsg { }
+
+    [JsonDiscriminator("job_process")]
+    public class JobProcessMsg : ClientMsg { }
+}
