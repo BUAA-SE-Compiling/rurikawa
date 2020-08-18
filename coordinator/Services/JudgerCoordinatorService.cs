0733a5ff331027a8d8c53cf6de7a44983fec5968
rami3l <rami3l@outlook.com>
2020-08-18 15:55:41 +0800

    feat: add skeleton for `Judger`

--- coordinator/Services/JudgerCoordinatorService.cs
+++ coordinator/Services/JudgerCoordinatorService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using Karenia.Rurikawa.Helpers;
@@ -10,11 +11,19 @@ using System.Threading.Channels;
 
 namespace Karenia.Rurikawa.Coordinator.Services {
     /// <summary>
-    /// A single-point coordinator for judgers
+    /// A single-point coordinator for judgers.
     /// </summary>
     public class JudgerCoordinatorService {
-        readonly Dictionary<string, JudgerState> connections = new Dictionary<string, JudgerState>();
+        /// <summary>
+        /// The collection of runners, with token as keys.
+        /// </summary>
+        readonly Dictionary<string, Judger> connections = new Dictionary<string, Judger>();
         readonly AsyncReaderWriterLock connectionLock = new AsyncReaderWriterLock();
+
+        /// <summary>
+        /// A channel indicating finished judgers' Id.
+        /// </summary>
+        private Channel<string> FinishedJudgers { get; } = Channel.CreateUnbounded<string>();
 
 
         // readonly HashSet<string> vacantJudgers = new HashSet<string>();
@@ -24,7 +33,9 @@ namespace Karenia.Rurikawa.Coordinator.Services {
                 if (await CheckAuth(auth)) {
                     var ws = await ctx.WebSockets.AcceptWebSocketAsync();
                     var wrapper = new JsonWebsocketWrapper<ClientMsg, ServerMsg>(ws);
-                    var judger = new JudgerState(wrapper);
+                    var judger = new Judger(auth, wrapper, this.FinishedJudgers);
+                    // Mark the judger as available.
+                    await judger.Finish();
                     using (await connectionLock.OpenWriter()) {
                         connections.Add(auth, judger);
                     }
@@ -46,30 +57,78 @@ namespace Karenia.Rurikawa.Coordinator.Services {
             return new ValueTask<bool>(true);
         }
 
-        public async Task AddNewJob() {
+        public async Task<int> HandleJob() {
             try {
                 using (await connectionLock.OpenWriter()) {
-                    var judger = connections.Where(x => x.Value.CanAcceptNewTask).First();
-                    judger.Value.CanAcceptNewTask = false;
-                    // TODO: Send task to this judger
+                    // Get an Id for a judger that is finished AND available.
+                    string judgerId;
+                    do {
+                        judgerId = await this.FinishedJudgers.Reader.ReadAsync();
+                    } while (!connections.ContainsKey(judgerId));
+
+                    using (await connectionLock.OpenReader()) {
+                        var judger = connections[judgerId];
+                        // TODO: Send task to judger
+                        var res = await judger.Run();
+                        return res;
+                    }
                 }
             } catch {
-                // No vacant judger
-                // TODO: Send task to database
+                // TODO: Other cases
+                throw new NotImplementedException();
             }
         }
     }
 }
 
-class JudgerState {
-    public JudgerState(JsonWebsocketWrapper<ClientMsg, ServerMsg> Socket) {
-        this.Socket = Socket;
-    }
 
-    public int ActiveTaskCount { get; set; } = 0;
-    public bool CanAcceptNewTask { get; set; } = true;
+/// <summary>
+/// A runner of a specific testing task.
+/// </summary>
+class Judger {
+
+    // public int ActiveTaskCount { get; set; } = 0;
+    // public bool CanAcceptNewTask { get; set; } = true;
     public JsonWebsocketWrapper<ClientMsg, ServerMsg> Socket { get; }
 
-    public async Task SendJob() { }
-}
+    /// <summary>
+    /// A channel to communicate with `JudgerState`,
+    /// and to indicate if this judger is finished.
+    /// </summary>
+    public Channel<string> Chan { get; }
 
+    public string Id { get => _id; }
+    private string _id;
+
+    public Judger(
+        string id,
+        JsonWebsocketWrapper<ClientMsg, ServerMsg> socket,
+        Channel<string> chan
+    ) {
+        this._id = id;
+        this.Socket = socket;
+        this.Chan = chan;
+    }
+
+    /// <summary>
+    /// Run a judger and get results.
+    /// </summary>
+    public async Task<int> Run() {
+        // TODO: Actually run the judger.
+        var rand = new Random();
+        var dur = rand.Next(2000);
+        // Run an expensive job.
+        await Task.Delay(dur);
+        // Send a signal to the channel when finished,
+        // indicating availability.
+        await this.Finish();
+        return 0;
+    }
+
+    /// <summary>
+    /// Tell the channel that the job is done.
+    /// </summary>
+    public async Task Finish() {
+        await this.Chan.Writer.WriteAsync(this.Id);
+    }
+}
