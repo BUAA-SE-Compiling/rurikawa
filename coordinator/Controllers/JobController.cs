65a81ddeb4961443a30e48ed693e06e3de469125
Rynco Maekawa <lynzrand@outlook.com>
2020-08-24 16:43:47 +0800

    feat: Add controller base

---
+++ coordinator/Controllers/JobController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Karenia.Rurikawa.Models;
+using Karenia.Rurikawa.Models.Judger;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Karenia.Rurikawa.Coordinator.Controllers {
+    [Route("api/v1/job/")]
+    public class JobController : ControllerBase {
+        public JobController(ILogger<JobController> logger, RurikawaDb db) {
+            this.logger = logger;
+            this.db = db;
+        }
+
+        private readonly ILogger<JobController> logger;
+        private readonly RurikawaDb db;
+
+
+        /// <summary>
+        /// GETs a job by its identifier
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public Job GetJob(ulong id) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// GETs a job by its identifier (stringified version)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public Job GetJob(string id) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// PUTs a new job
+        /// </summary>
+        /// <param name="job"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public string NewJob(Job job) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ coordinator/Controllers/JudgerApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Karenia.Rurikawa.Coordinator.Controllers {
+    [ApiController]
+    [Route("api/v1/judger/")]
+    public class JudgerApiController : ControllerBase {
+        private readonly ILogger<JudgerApiController> _logger;
+
+        public JudgerApiController(ILogger<JudgerApiController> logger) {
+            _logger = logger;
+        }
+
+    }
+}
