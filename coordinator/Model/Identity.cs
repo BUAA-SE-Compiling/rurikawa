c3f830274d4c623eee13f778165174ebcd5ba27d
Rynco Maekawa <lynzrand@outlook.com>
2020-08-28 17:40:47 +0800

    style: disable CS8618 in specific codes

--- coordinator/Model/Identity.cs
+++ coordinator/Model/Identity.cs
@@ -4,6 +4,7 @@ using System.Text.Json.Serialization;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Karenia.Rurikawa.Models.Auth {
+#pragma warning disable CS8618
     public class OAuth2Request {
         public string GrantType { get; set; }
         public string Scope { get; set; }
@@ -28,4 +29,5 @@ namespace Karenia.Rurikawa.Models.Auth {
     public static class AuthConstants {
         public static readonly string WebhookScope = "";
     }
+#pragma warning restore
 }
