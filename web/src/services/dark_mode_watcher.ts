0e632f7c90d5ec93aa7bd12fac60bfbf5d0d62cc
Rynco Maekawa <lynzrand@outlook.com>
2020-10-29 00:28:18 +0800

    feat: add dark-mode-specific icons

--- web/src/services/dark_mode_watcher.ts
+++ web/src/services/dark_mode_watcher.ts
@@ -28,12 +28,43 @@ export class DarkModeWatcher {
     darkModeWatcherInstance = this;
   }
 }
+
+const lightModeAssets = {
+  appleTouchIcon: 'assets/apple-touch-icon.png',
+  icon32: 'assets/favicon-32x32.png',
+  icon16: 'assets/favicon-16x16.png',
+  iconIco: 'assets/favicon.ico',
+};
+
+const darkModeAssets = {
+  appleTouchIcon: 'assets/darkmode/apple-touch-icon.png',
+  icon32: 'assets/darkmode/favicon-32x32.png',
+  icon16: 'assets/darkmode/favicon-16x16.png',
+  iconIco: 'assets/darkmode/favicon.ico',
+};
+
 function onLightMode() {
   document.getElementsByTagName('body')[0].classList.add('default-palette');
   document.getElementsByTagName('body')[0].classList.remove('inverted-palette');
+  (document.getElementById('apple-touch-icon') as HTMLLinkElement).href =
+    lightModeAssets.appleTouchIcon;
+  (document.getElementById('icon-32') as HTMLLinkElement).href =
+    lightModeAssets['icon32'];
+  (document.getElementById('icon-16') as HTMLLinkElement).href =
+    lightModeAssets['icon16'];
+  (document.getElementById('icon-ico') as HTMLLinkElement).href =
+    lightModeAssets['iconIco'];
 }
 
 function onDarkMode() {
   document.getElementsByTagName('body')[0].classList.remove('default-palette');
   document.getElementsByTagName('body')[0].classList.add('inverted-palette');
+  (document.getElementById('apple-touch-icon') as HTMLLinkElement).href =
+    darkModeAssets.appleTouchIcon;
+  (document.getElementById('icon-32') as HTMLLinkElement).href =
+    darkModeAssets['icon32'];
+  (document.getElementById('icon-16') as HTMLLinkElement).href =
+    darkModeAssets['icon16'];
+  (document.getElementById('icon-ico') as HTMLLinkElement).href =
+    darkModeAssets['iconIco'];
 }
